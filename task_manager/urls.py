ea7640323346fd891e37a8c6825f880344eb8fa3
martoff <martoff@ukr.net>
2026-06-23 21:55:23 +0300

    create urls for task_manager

---
+++ task_manager/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from task_manager.views import (
+    IndexView,
+    TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
+
+app_name = "task_manager"
+
+urlpatterns = [
+    # Main page
+    path("", IndexView.as_view(), name="index"),
+    
+    # Tasks (CRUD)
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+]
