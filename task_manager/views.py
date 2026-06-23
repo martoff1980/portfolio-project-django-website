a8edbc43d22630b214e6756ff41b82b15a761e5a
martoff <martoff@ukr.net>
2026-06-23 13:57:51 +0300

    create views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Task
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "task_manager/task_list.html"
+    context_object_name = "task_list"
+    paginate_by = 10
+
+    def get_queryset(self):
+        #  We can implement filtering or searching by task name
+        queryset = Task.objects.select_related().prefetch_related("assignees", "tags")
+        query = self.request.GET.get("name")
+        if query:
+            return queryset.filter(name__icontains=query)
+        return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+    template_name = "task_manager/task_detail.html"
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+    template_name = "task_manager/task_confirm_delete.html"
