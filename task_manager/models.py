f8a82b26d64272269e5b9139cc22c38c8da73102
martoff <martoff@ukr.net>
2026-06-24 01:10:27 +0300

    add models Team and Project, update Task for connect with Project model

--- task_manager/models.py
+++ task_manager/models.py
@@ -27,10 +27,29 @@ class Worker(AbstractUser):
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self) -> str:
         return self.name
 
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    # За каждым проектом закреплена одна команда (или наоборот)
+    team = models.ForeignKey(
+        Team, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name="projects"
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -49,10 +68,35 @@ class Task(models.Model):
         choices=PRIORITY_CHOICES, 
         default="medium"
     )
+    
+    project = models.ForeignKey(
+        Project, 
+        on_delete=models.CASCADE, 
+        related_name="tasks",
+        # null=True,temporary, 
+        # to avoid errors with old tasks in the database
+        null=True,  
+        blank=False
+    )
+    
     # Connection for workers who execising task
     assignees = models.ManyToManyField(Worker, related_name="tasks")
     # Connection for tags (option)
     tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
-
+    
+    class Meta:
+        ordering = ["deadline"]
+        
     def __str__(self) -> str:
         return f"{self.name} (Completed: {self.is_completed})"
+    
+class Team(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    #Members of the team (Many-to-Many with Worker)
+    members = models.ManyToManyField(Worker, related_name="teams")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
