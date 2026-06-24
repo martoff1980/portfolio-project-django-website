18dc477ecc80b3b3736515b4fce28772a3bb98d1
martoff <martoff@ukr.net>
2026-06-24 12:51:10 +0300

    style: apply flake8 formatting

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 
 from task_manager.models import Position, Worker, Tag, Task, Team, Project
 
+
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ["name"]
@@ -13,19 +14,25 @@ class PositionAdmin(admin.ModelAdmin):
 class WorkerAdmin(UserAdmin):
     # Add field position in users list
     list_display = UserAdmin.list_display + ("position",)
-    
+
     # Add filter by position in the right panel
     list_filter = ["position"]
-    
-    # Set the fieldsets to include the position field in the user edit form
+
+    # Set the fieldsets to include the position field
+    # in the user edit form
     fieldsets = UserAdmin.fieldsets + (
         (("Additional info"), {"fields": ("position",)}),
     )
 
-    # Set the add_fieldsets to include the position field 
+    # Set the add_fieldsets to include the position field
     # in the user creation form
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (("Additional info"), {"fields": ("first_name", "last_name", "email", "position")}),
+        (("Additional info"), {"fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "position"
+        )}),
     )
 
 
@@ -39,17 +46,18 @@ class TagAdmin(admin.ModelAdmin):
 class TaskAdmin(admin.ModelAdmin):
     # Fields that will be visible in the list of all tasks
     list_display = ["name", "deadline", "priority", "is_completed", "project"]
-    
+
     # Filter by completion status, priority, deadline, and tags
     list_filter = ["is_completed", "priority", "deadline", "tags", "project"]
-    
+
     # Search by task name and description
     search_fields = ["name", "description"]
-    
+
     # This allows for a better user experience
     # when selecting multiple assignees or tags for a task.
     filter_horizontal = ["assignees", "tags"]
-    
+
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ["name"]
